3707095c6009d577da415c88efdc480cb928c043
Reverend Lovetta <reverend@oa-j2.msk.21-school.ru>
2022-06-30 21:36:10 +0300

    Добавляю s21_cat.h и инициализирую структуру s21_cat.c

---
+++ src/cat/s21_cat.h
@@ -0,0 +1,15 @@
+#ifndef SRC_S21_CAT_H_
+#define SRC_S21_CAT_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct flags {
+    int b;
+    int e;
+    int v;
+    int n;
+    int s;
+    int t;
+} flags;
+
+#endif  // SRC_S21_CAT_H_
