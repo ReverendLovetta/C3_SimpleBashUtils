578c7d8e3fc1e7b2aaa82d4818b847b6cfcc9855
Reverend Lovetta <reverend@oa-f3.msk.21-school.ru>
2022-07-19 21:04:32 +0300

    Дописываю реализацию функции s21_reading_parameters для linux

--- src/cat/s21_reading_parameters.c
+++ src/cat/s21_reading_parameters.c
@@ -35,11 +35,51 @@ void reading_parameters(int* argc, char* argv[], flags* param) {
                 fprintf(stderr, "Неизвестный параметр: '%s'\n", optarg);
                 exit(1);
             }
+            *argc = *argc - optind;
+            argv += optind;
         }
 
     }
     #endif  // __APPLE__
     #ifdef __linux__
-    
+    const char* short_options = "benstET";
+    const struct option long_options[] = {
+        {"number-nonblank", no_argument, NULL, 'b'},
+        {"number", no_argument, NULL, 'n'},
+        {"squeeze-blank", no_argument, NULL, 's'},
+        {NULL, 0, NULL, 0}
+    };
+    int res = 0;
+    int option_index = -1;
+    while ((res = getopt_long(*argc, argv, short_options, long_options, &option_index)) != -1) {
+        switch (res) {
+            case 'b': {
+                param->b = 1;
+                break;
+            }
+            case 'e': {
+                param->e = 1;
+                param->v = 1;
+                break;
+            }
+            case 'n': {
+                param->n = 1;
+                break;
+            }
+            case 's': {
+                param->s = 1;
+                break;
+            }
+            case 't': {
+                param->t = 1;
+                param->v = 1;
+                break;
+            }
+            default: {
+                fprintf(stderr, "Неизвестный параметр: '%s'\n", optarg);
+                exit(1);
+            }
+        }
+    }
     #endif  // __linux__
 }
