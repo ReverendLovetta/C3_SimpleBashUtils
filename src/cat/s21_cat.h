500f679b986eb6ba3caed0a8489ea61a7970adda
Reverend Lovetta <reverend@oa-f3.msk.21-school.ru>
2022-07-20 16:47:46 +0300

    Дописал функцию для заполнения структуры: s21_reading_parameters, добавил makefile + небольшой текстовый файл для тестов

--- src/cat/s21_cat.h
+++ src/cat/s21_cat.h
@@ -1,19 +1,19 @@
 #ifndef SRC_S21_CAT_H_
 #define SRC_S21_CAT_H_
 
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <getopt.h>
 
 typedef struct flags {
-    int b;
-    int e;
-    int v;
-    int n;
-    int s;
-    int t;
+  int b;
+  int e;
+  int v;
+  int n;
+  int s;
+  int t;
 } flags;
 
-void reading_parameters(int* argc, char* argv[], flags* param);
+char** reading_parameters(int* argc, char* argv[], flags* param);
 
 #endif  // SRC_S21_CAT_H_
--- src/cat/s21_reading_parameters.c
+++ src/cat/s21_reading_parameters.c
@@ -1,85 +1,88 @@
-/* Функция void reading_parameters(int* n, char** agrv, flags* param). 
+/* Функция void reading_parameters(int* n, char** agrv, flags* param).
  * Заполняет структуру start флагами из stdin.
  */
 
-#include <s21_cat.h>
+#include "s21_cat.h"
 
-void reading_parameters(int* argc, char* argv[], flags* param) {
-    #ifdef __APPLE__
-    char ch;
-    while ((ch = getopt(*argc, argv, "benst")) != EOF) {
-        switch (ch) {
-            case 'b': {
-                param->b = 1;
-                break;
-            }
-            case 'e': {
-                param->e = 1;
-                param->v = 1;
-                break;
-            }
-            case 'n': {
-                param->n = 1;
-                break;
-            }
-            case 's': {
-                param->s = 1;
-                break;
-            }
-            case 't': {
-                param->t = 1;
-                param->v = 1;
-                break;
-            }
-            default: {
-                fprintf(stderr, "Неизвестный параметр: '%s'\n", optarg);
-                exit(1);
-            }
-            *argc = *argc - optind;
-            argv += optind;
-        }
-
+char** reading_parameters(int* argc, char* argv[], flags* param) {
+#ifdef __APPLE__
+  char ch;
+  while ((ch = getopt(*argc, argv, "benst")) != EOF) {
+    switch (ch) {
+      case 'b': {
+        param->b = 1;
+        break;
+      }
+      case 'e': {
+        param->e = 1;
+        param->v = 1;
+        break;
+      }
+      case 'n': {
+        param->n = 1;
+        break;
+      }
+      case 's': {
+        param->s = 1;
+        break;
+      }
+      case 't': {
+        param->t = 1;
+        param->v = 1;
+        break;
+      }
+      default: {
+        fprintf(stderr, "Неизвестный параметр: '%s'\n", optarg);
+        exit(1);
+      }
     }
-    #endif  // __APPLE__
-    #ifdef __linux__
-    const char* short_options = "benstET";
-    const struct option long_options[] = {
-        {"number-nonblank", no_argument, NULL, 'b'},
-        {"number", no_argument, NULL, 'n'},
-        {"squeeze-blank", no_argument, NULL, 's'},
-        {NULL, 0, NULL, 0}
-    };
-    int res = 0;
-    int option_index = -1;
-    while ((res = getopt_long(*argc, argv, short_options, long_options, &option_index)) != -1) {
-        switch (res) {
-            case 'b': {
-                param->b = 1;
-                break;
-            }
-            case 'e': {
-                param->e = 1;
-                param->v = 1;
-                break;
-            }
-            case 'n': {
-                param->n = 1;
-                break;
-            }
-            case 's': {
-                param->s = 1;
-                break;
-            }
-            case 't': {
-                param->t = 1;
-                param->v = 1;
-                break;
-            }
-            default: {
-                fprintf(stderr, "Неизвестный параметр: '%s'\n", optarg);
-                exit(1);
-            }
-        }
+    *argc = *argc - optind;
+    argv += optind;
+    return argv;
+  }
+#endif  // __APPLE__
+#ifdef __linux__
+  const char* short_options = "benstET";
+  const struct option long_options[] = {
+      {"number-nonblank", no_argument, NULL, 'b'},
+      {"number", no_argument, NULL, 'n'},
+      {"squeeze-blank", no_argument, NULL, 's'},
+      {NULL, 0, NULL, 0}};
+  int res = 0;
+  int option_index = -1;
+  while ((res = getopt_long(*argc, argv, short_options, long_options,
+                            &option_index)) != -1) {
+    switch (res) {
+      case 'b': {
+        param->b = 1;
+        break;
+      }
+      case 'e': {
+        param->e = 1;
+        param->v = 1;
+        break;
+      }
+      case 'n': {
+        param->n = 1;
+        break;
+      }
+      case 's': {
+        param->s = 1;
+        break;
+      }
+      case 't': {
+        param->t = 1;
+        param->v = 1;
+        break;
+      }
+      default: {
+        fprintf(stderr, "Неизвестный параметр: '%s'\n", optarg);
+        exit(1);
+      }
     }
-    #endif  // __linux__
+  }
+  *argc = *argc - optind;
+  *argv += optind;
+  return argv;
+#endif  // __linux__
 }
