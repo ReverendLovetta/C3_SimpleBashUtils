974ed6e79c3aca47d25b9fa524da17fbcc5cecf1
reverend <reverend@student.21-school.ru>
2022-08-04 21:40:11 +0300

    Добавляю поддержку фала -b

--- src/cat/s21_cat_use_flags.c
+++ src/cat/s21_cat_use_flags.c
@@ -12,20 +12,18 @@ void s21_cat_use_flags(flags* param, FILE *file) {
 #endif  // __APPLE__
 #ifdef __linux__
   int c = 0;
-  static int i = 0;
+  static int i = 1;
   //static int j = 0;
 #endif  // __linux__
   char temp1 = '\0';
-  char temp2 = '\0';
+  //char temp2 = '\0';
   char flag_first_str = '0';
   while ((c = fgetc(file)) != EOF) {
     if (param->b == 1) {
       if (param->n == 1) {
         param->n = 0;
       }
-      if ((temp1 == '\n' && temp2 == '\n' && flag_first_str != '0')) {
-        continue;
-      } else {
+      if ((temp1 == '\n' && c != '\n') || flag_first_str == '0') {
         printf("%6d\t", i);
         i++;
       }
@@ -40,7 +38,7 @@ void s21_cat_use_flags(flags* param, FILE *file) {
     // } else if (param->v == 1) {
     // }
     printf("%c", c);
-    temp2 = temp1;
+    //temp2 = temp1;
     temp1 = c;
     if (flag_first_str == '0') {
       flag_first_str = '1';
